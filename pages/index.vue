2cff7d3e514d8d0a563a72d3dce5b856b2a45583
BlackWolf96 <oskar.blackwolf@gmail.com>
2022-03-14 10:56:36 +0100

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -6,15 +6,42 @@
         <h2>
           W naszej ofercie znajdziecie Państwo:
         </h2>
+        <ul>
+          <li>Montaż okien</li>
+          <li>Montaż drzwi</li>
+          <li>Montaż parapetów</li>
+          <li>Montaż bram garażowych</li>
+          <li>Montaż systemów balkonowych</li>
+          <li>Montaż ogrodów zimowych</li>
+          <li>Montaż Fasad</li>
+          <li>Montaż systemów Ppoż</li>
+        </ul>
       </article>
       <article class="container__article">
         <h2>
-          W naszej ofercie znajdziecie Państwo:
+          Rodzaje montażu:
         </h2>
+        <h3>Montaż surowy:</h3>
+        <p>Surowy montaż okien stosowany jest w przypadku przypadku budynków w stanie surowym</p>
+        <h3>Montaż pełny:</h3>
+        <p>Pełny montaż okien stosowany w przypadku budynków, w krórych okna zostaną wymienione</p>
+        <h3>Montaż ciepły</h3>
+        <p>Polega na zastosowaniu trzech warstw składających się z:
+        </p>
+        <ul>
+          <li>Zewnętrznej</li>
+          <li>Środkowej</li>
+          <li>Wewnętrznej</li>
+        </ul>
+        <p>Warstwa zewnętrzna chroni izolację przed wilgocią z zewnątrz. Warstwa środkowa służąca jako izolacja termiczna wykonana jest z poliuretanowej piany, która szczelnie wypełnia przestrzń pomiędzy ramą okna a ścianą. Warstwa wewnętrzna chroni przed wnikaniem pary wodnej zbierającej się w pomieszczeniach w izolacje.</p>
       </article>
     </section>
-    <section class="flex"></section>
-    <section class="flex"></section>
+    <section class="flex">
+
+    </section>
+    <section class="flex">
+
+    </section>
   </div>
 </template>
 
