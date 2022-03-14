6945e34618905d4a10650c7427b0092bbb0ba8bb
BlackWolf96 <oskar.blackwolf@gmail.com>
2022-03-14 10:48:54 +0100

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,16 @@
   <div>
     <Navbar />
     <section class="flex container">
-      <article class="container--article">
+      <article class="container__article">
         <h2>
           W naszej ofercie znajdziecie Państwo:
         </h2>
       </article>
-      <article class="container--article">2</article>
+      <article class="container__article">
+        <h2>
+          W naszej ofercie znajdziecie Państwo:
+        </h2>
+      </article>
     </section>
     <section class="flex"></section>
     <section class="flex"></section>
