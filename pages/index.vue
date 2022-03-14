baa1a61396807975ff21028804e107d88a8b429b
BlackWolf96 <oskar.blackwolf@gmail.com>
2022-03-14 18:08:24 +0100

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -39,7 +39,7 @@
     <section class="flex container margin__top--150">
       <img src="https://www.renko.pl/_nuxt/img/renko3.45acdd1.webp" class="container__image">
       <div class="container__box bg__black--transparent">1</div>
-      <div class="container_box bg__black--transparent">2</div>
+      <div class="container__box bg__black--transparent">2</div>
       <div class="container__box bg__black--transparent">3</div>
     </section>
     <section class="flex">
