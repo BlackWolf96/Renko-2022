c0a7bd7842662ea62d634d0dbe9d2bcd9e214ac8
BlackWolf96 <oskar.blackwolf@gmail.com>
2022-03-14 10:10:20 +0100

    Update

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: '/favicon.ico' }
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;1,100;1,300;1,400&display=swap' }
     ]
   },
 
