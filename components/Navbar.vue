2e93a5f83047cb9d004187b0bcf18401edb42bbe
BlackWolf96 <oskar.blackwolf@gmail.com>
2022-03-14 01:40:49 +0100

    Update

---
+++ components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="navigation">
+    <nav class="navigation__nav">
+      <nuxt-link to="/" class="navigation__link">
+        Home
+      </nuxt-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
